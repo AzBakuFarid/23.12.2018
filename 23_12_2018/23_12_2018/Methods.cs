067811ce20f045b02e8850531031cbb16dedb6cf
AzBakuFarid <40527149+AzBakuFarid@users.noreply.github.com>
2018-12-23 19:50:27 +0400

    Add files via upload

---
+++ 23_12_2018/23_12_2018/Methods.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_12_2018
+{
+    class Methods
+    {
+        public static object Locker = new object();
+
+        public static async Task<Stack<string>> readFromFileAsync(string filename)
+        {
+            Stack<string> list = new Stack<string>();
+            await Task.Run(() =>
+            {
+                using (StreamReader sr = new StreamReader(filename))
+                {
+                    string line = null;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        list.Push(line);
+                    }
+                }
+            });
+            return list;
+
+
+
+        }
+        public static async Task writeToDB(Stack<string> stack)
+        {
+            string sb = null;
+            Task task1 = Task.Run(() =>
+            {
+                if (stack.Count > 0)
+                {
+                    lock (Locker)
+                    {
+                        SqlConnection conn = new SqlConnection(@"Data Source=CACR12\SERVER2018;User id=sa; Password=1Qaz2wsx; Initial Catalog=items_23/12/2018");
+                        conn.Open();
+                        sb = $"insert into items (items) values ('{stack.Pop()}'); ";
+                        SqlCommand cmd = new SqlCommand(sb.ToString(), conn);
+                        var resultI = cmd.ExecuteNonQuery();
+                        conn.Close();
+                    }
+
+                }
+
+            });
+            Task task4 = Task.Run(() =>
+            {
+                if (stack.Count > 0)
+                {
+                    lock (Locker)
+                    {
+                        SqlConnection conn = new SqlConnection(@"Data Source=CACR12\SERVER2018;User id=sa; Password=1Qaz2wsx; Initial Catalog=items_23/12/2018");
+                        conn.Open();
+                        sb = $"insert into items (items) values ('{stack.Pop()}'); ";
+                        SqlCommand cmd = new SqlCommand(sb.ToString(), conn);
+                        var resultI = cmd.ExecuteNonQuery();
+                        conn.Close();
+                    }
+
+                }
+
+            });
+            Task task5 = Task.Run(() =>
+            {
+                if (stack.Count > 0)
+                {
+                    lock (Locker)
+                    {
+                        SqlConnection conn = new SqlConnection(@"Data Source=CACR12\SERVER2018;User id=sa; Password=1Qaz2wsx; Initial Catalog=items_23/12/2018");
+                        conn.Open();
+                        sb = $"insert into items (items) values ('{stack.Pop()}'); ";
+                        SqlCommand cmd = new SqlCommand(sb.ToString(), conn);
+                        var resultI = cmd.ExecuteNonQuery();
+                        conn.Close();
+                    }
+
+                }
+
+            });
+            Task task2 = Task.Run(() =>
+            {
+                if (stack.Count > 0)
+                {
+                    lock (Locker)
+                    {
+                        SqlConnection conn = new SqlConnection(@"Data Source=CACR12\SERVER2018;User id=sa; Password=1Qaz2wsx; Initial Catalog=items_23/12/2018");
+                        conn.Open();
+                        sb = $"insert into items (items) values ('{stack.Pop()}'); ";
+                        SqlCommand cmd = new SqlCommand(sb.ToString(), conn);
+                        var resultI = cmd.ExecuteNonQuery();
+                        conn.Close();
+                    }
+
+                }
+
+            });
+            Task task3 = Task.Run(() =>
+            {
+                if (stack.Count > 0)
+                {
+                    lock (Locker)
+                    {
+                        SqlConnection conn = new SqlConnection(@"Data Source=CACR12\SERVER2018;User id=sa; Password=1Qaz2wsx; Initial Catalog=items_23/12/2018");
+                        conn.Open();
+                        sb = $"insert into items (items) values ('{stack.Pop()}'); ";
+                        SqlCommand cmd = new SqlCommand(sb.ToString(), conn);
+                        var resultI = cmd.ExecuteNonQuery();
+                        conn.Close();
+                    }
+
+                }
+
+            });
+
+            Task.WaitAll(task1,task2,task3,task4,task5); 
+
+        }
+    }
+}
+
+
---
+++ 23_12_2018/23_12_2018/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_12_2018
+{   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var isWriting = true;
+            var items = Methods.readFromFileAsync(@"C:\Users\FaridAz\Desktop\stack_items.txt");
+            while (isWriting)
+            {
+                Methods.writeToDB(items.Result);
+                if (items.Result.Count==0) { isWriting = false; }
+            }
+        }
+    }
+}
